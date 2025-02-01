7c4ce842938a88f627c94d7e1570453d35f6567d
Sahil Baig <sahilbaig.me@gmail.com>
2025-02-01 18:25:50 +0530

    removed errror routes

--- app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Resend } from "resend";
-import { NextResponse } from "next/server";
-
-const resend = new Resend(process.env.API_KEY);
-const what = () => {
-  return NextResponse.json({ hashas: "asas" });
-};
